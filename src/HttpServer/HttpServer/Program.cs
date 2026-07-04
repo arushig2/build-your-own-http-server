03226a1e79d01296111187a9eb0fe5bcfa7c630e
Arushi Gupta <arushig223@gmail.com>
2026-07-04 19:31:16 +0530

    Add: implement TCP listener for incoming connections

--- src/HttpServer/HttpServer/Program.cs
+++ src/HttpServer/HttpServer/Program.cs
@@ -1,10 +1,6 @@
-﻿namespace HttpServer
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System;
+using HttpServer;
+
+var server = new Server();
+
+server.Start();
---
+++ src/HttpServer/HttpServer/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+
+namespace HttpServer
+{
+    public class Server
+    {
+
+        public void Start()
+        {
+            const int Port = 8080;
+            var ipEndPoint = new IPEndPoint(IPAddress.Any, Port);
+            TcpListener listener = new(ipEndPoint);
+            Console.WriteLine("Starting server...");
+            try
+            {
+                
+                listener.Start();
+                Console.WriteLine($"Listening on port {Port}...");
+                Console.WriteLine("Waiting for a client to connect...");
+
+                var client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected.");
+            }
+            finally
+            {
+                listener.Stop();
+                Console.WriteLine($"Stopped listening on port {Port}...");
+            }
+        }
+    }
+}
