4cdd7843f8747c7989662839505602bfe1d4d04b
Arushi Gupta <arushig223@gmail.com>
2026-07-05 19:26:37 +0530

    Add: implement HTTP response handling and basic routing

--- src/HttpServer/HttpServer/Server.cs
+++ src/HttpServer/HttpServer/Server.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection.Metadata;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
 
 namespace HttpServer
 {
@@ -32,7 +35,38 @@ namespace HttpServer
                 string dataRead = Encoding.UTF8.GetString(buffer, 0, bytesRead);
                 Console.WriteLine($"Data recieved : {dataRead}");
 
+                
+                string requestLine = dataRead.Split("\n")[0];
+                string route = requestLine.Split(" ")[1];
 
+
+                string body = "Invalid Route";
+
+                if (route == "/")
+                {
+                    body = "Home Page";
+                }
+                else if (route == "/about")
+                {
+                    body = "About Page";
+                }
+                else if (route == "/contact")
+                {
+                    body = "Contact us at example@email.com";
+                }
+
+                int numBytes = Encoding.UTF8.GetByteCount(body);
+
+                string response = $"""
+                                  HTTP/1.1 200 OK
+                                  Content-Type: text/html; charset=utf-8
+                                  Content-Length: {numBytes}
+
+                                  {body}
+                                  """;
+
+               byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+               stream.Write(responseBytes, 0, responseBytes.Length);
 
             }
             finally
